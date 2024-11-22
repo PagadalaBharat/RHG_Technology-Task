cefe9d20240a365d82e065ccd6cddfd40131ada9
PagadalaBharat <pagadalaraviteja73@gmail.com>
2024-11-22 20:22:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('open');
+});
+
+
+var typed = new Typed(".auto-type", {
+  strings: ["ScreenWise", "fulfillment in real-life moments"],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true,
+});
+
+
+
